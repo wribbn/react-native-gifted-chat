f863538e883379ee5f7614a056803c02cb40a288
Nick Badagliacca <nick@terraeclipse.com>
2019-02-05 10:47:23 -0800

    Allow a custom input component for Composer

--- src/Composer.js
+++ src/Composer.js
@@ -42,8 +42,10 @@ export default class Composer extends React.Component {
   }
 
   render() {
+    const InputComponent = this.props.inputComponent || TextInput
+
     return (
-      <TextInput
+      <InputComponent
         testID={this.props.placeholder}
         accessible
         accessibilityLabel={this.props.placeholder}
